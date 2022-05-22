9802f1f0ec4be70d91094bcbd4dc14d4da897d62
AB070DA <105996472+AB070DA@users.noreply.github.com>
2022-05-22 06:07:40 +0200

    Add files via upload

---
+++ StudentDepartment.java
@@ -0,0 +1,37 @@
+
+
+public class StudentDepartment {
+
+	// Attributes
+	private String department;
+	private int code;
+	
+	//Constructor
+	public StudentDepartment() {
+		department = null;
+		code = 0;
+	}
+
+	public StudentDepartment(String department, int code) {
+		this.department = department;
+		this.code = code;
+	}
+
+	//Methods
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+}
