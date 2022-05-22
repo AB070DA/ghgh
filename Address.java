8454002e4607e2be2f0f6f741c3359b66170591d
AB070DA <105996472+AB070DA@users.noreply.github.com>
2022-05-22 03:52:49 +0200

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,104 @@
+
+public class Address {
+	
+	//attributes
+	private int flateNumber;
+	private int houseNumber;
+	private int postalCode;
+	private String streetName;
+	private String areaName;
+	private String cityName;
+	private String governorateName;
+	private String country;
+	
+	// no arguments constructor
+	public Address() {
+		flateNumber = 0;
+		houseNumber = 0;
+		postalCode = 0;
+		streetName = null;
+		areaName = null;
+		cityName = null;
+		governorateName = null;
+		country = null;
+	}
+	
+	//arguments constructor
+	public Address(int flateNumber, int houseNumber, int postalCode, String streetName, String areaName,
+			String cityName, String governorateName, String country) {
+		this.flateNumber = flateNumber;
+		this.houseNumber = houseNumber;
+		this.postalCode = postalCode;
+		this.streetName = streetName;
+		this.areaName = areaName;
+		this.cityName = cityName;
+		this.governorateName = governorateName;
+		this.country = country;
+	}
+	
+	// Methods
+	public void setFlateNumber(int flateNumber) {
+		this.flateNumber = flateNumber;
+	}
+	
+	public int getFlateNumber() {
+		return flateNumber;
+	}
+	
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+	
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public int getPostalCode() {
+		return postalCode;
+	}
+	
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	
+	public String getStreetName() {
+		return streetName;
+	}
+	
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+
+	public String getAreaName() {
+		return areaName;
+	}
+	
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setGovernorateName(String governorateName) {
+		this.governorateName = governorateName;
+	}
+
+	public String getGovernorateName() {
+		return governorateName;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+}
