356c103b098759178baf5823ae72397b6eac2026
AB070DA <105996472+AB070DA@users.noreply.github.com>
2022-05-22 02:19:43 +0200

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,38 @@
+
+public class Name {
+	//attributes
+	String firstName;
+	String middleName;
+	String lastName;
+	// constructor with arguments
+	public Name(String first,String middle,String last) {
+		firstName = first;
+		middleName=middle;
+		lastName= last;
+	}
+	//set first name
+	public void setFirstName(String firstName) {
+		this.firstName=firstName;
+	}
+	// set middle name
+	public void setMiddleName(String middleName) {
+		this.middleName=middleName;
+	}
+	// set last name
+	public void setLastName(String lastName) {
+		this.lastName=lastName;
+	}
+	//get first name
+	public String getFirstName() {
+		return firstName;
+	}
+	// get middle name
+	public String getMiddleName() {
+		return middleName;
+	}
+	// get last name
+	public String getLastName() {
+		return lastName;
+	}
+
+}
